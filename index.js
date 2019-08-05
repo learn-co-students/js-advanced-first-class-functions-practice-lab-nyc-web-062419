161a9eaed2f185ff4e0b4bbb019d80cf420879ee
Ben Braunstein <benzbraunstein@gmail.com>
2019-08-05 16:19:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function(d) {
+    console.log(d.name);
+  });
+}
+
+function logDriversByHometown(drivers, town) {
+  drivers.forEach(function(d) {
+    if (d.hometown === town) {
+      console.log(d.name);
+    }
+  });
+}
+
+let driversByRevenue = function(drivers) {
+  return drivers.slice().sort(function(d1, d2) {
+    return d1.revenue - d2.revenue;
+  });
+};
+
+let driversByName = function(drivers) {
+  return drivers.slice().sort(function(d1, d2) {
+    return d1.name.localeCompare(d2.name);
+  });
+};
+
+let totalRevenue = function(drivers) {
+  return drivers.reduce(function(total, d) {
+    return d.revenue + total;
+  }, 0);
+};
+
+function averageRevenue(drivers) {
+  let total = totalRevenue(drivers);
+  return total / drivers.length;
+}
